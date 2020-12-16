0733a9f40a4f66c519df0b7f5d59f7e6006b4cad
Ambar <ambarmogg@me.com>
2020-12-15 16:51:37 -0800

    add new travel route

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Travels from '../views/Travels.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -17,13 +18,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/travels',
+    name: 'travels',
+    component: Travels
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/Travels.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="travels">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Travel Page!",
+    };
+  },
+  created: function () {},
+  methods: {},
+};
+</script>
