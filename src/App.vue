2b57df7230686f26607b4ff4d1cae1723819ba5a
Ambar <ambarmogg@me.com>
2020-12-16 18:09:54 -0800

    add bootstrap and customize font and background iamge

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/posts">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/travels">Travels</router-link> |
-      <router-link to="/signup">Signup</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/posts">Posts Index</router-link> |
-      <router-link to="/posts/new">Create a new Post</router-link> |
-    </div>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="/posts">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          User
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/signup">Signup</a>
+          <a class="dropdown-item" href="/login">Login</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link create" href="/posts/new" tabindex="-1" aria-disabled="false">Create a Post</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     <router-view/>
   </div>
 </template>
 
 <style>
+body {
+  background-image: url("geometric-leaves.png");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Big Caslon, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
